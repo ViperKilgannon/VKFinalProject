ff13489679c212632e60e52e373c827aefe5f93c
Nigel Simms <nigel.simms@easternstudent.ca>
2020-01-23 11:21:34 -0400

    Initial setup

---
+++ src/components/LoginBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input v-model="username" />
+    <input v-model="password" type="password" />
+    <button @click="login">Login</button>
+    <div>{{ res }}</div>
+  </div>
+</template>
+
+
+<script>
+import Jquery from "jquery";
+
+const $ = Jquery;
+const server = "https://supernotes.duckdns.org";
+
+export default {
+  data: () => ({
+    username: "",
+    password: "",
+    res: ""
+  }),
+  methods: {
+    login() {
+      let dataToSend = {
+        username: this.username,
+        password: this.password
+      };
+      $.post(server + "/login", dataToSend, res => {
+        this.res = JSON.stringify(res);
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  border: 2px solid grey;
+  border-radius: 10px;
+  padding: 10px;
+}
+</style>
